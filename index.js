3f34fd85f512beb6bcfd6deb22464fabb3e6e86f
Cral_Cactus <58943239+Cral-Cactus@users.noreply.github.com>
2022-01-10 10:23:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Checker = require('./checker');
-const keyword = process.argv.slice(2)[0];
+const keyword = process.argv.slice(2)[0]; // Keywords
 const checker = new Checker({ keyword, saveAvailable: true });
 
 checker.check();
